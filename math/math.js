2cf240fc6bfa8447b801b552a79fab610c4b9333
JuanDa-code <juandax123456@gmail.com>
2023-03-09 19:25:04 -0500

    Circle calculation

--- math/math.js
+++ math/math.js
@@ -50,3 +50,34 @@ function calcularTriangulo(lado1, lado2, base, altura) {
 }
 
 console.groupEnd('Triangulo');
+
+/* Circulo */
+
+console.group('Circulo');
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circunferencia,
+    areaCirculo,
+});
+
+function calcularCirculo(radio) {
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+
+    return {
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI,
+    }
+}
+
+console.groupEnd('Circulo');
